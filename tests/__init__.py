40cc415daab2ab1ec1239b731ece2ca24a36f9c1
sonijha1522 <68427061+sonijha1522@users.noreply.github.com>
2025-06-05 23:37:23 +0530

    __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,1 @@
-from src.math_operation import add, sub
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
-    
-def test_sub(): 
-    assert sub(2, 1) == 1
-    assert sub(1, 1) == 0
-    assert sub(0, 1) == -1
